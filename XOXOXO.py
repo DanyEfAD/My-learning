2bbbb27ad0d3ebc12070c6306ec7ef775e2a093c
DanyEfAD <danya170298@yandex.ru>
2025-01-25 10:16:15 +0300

    now working

--- XOXOXO.py
+++ XOXOXO.py
@@ -1,14 +1,15 @@
-tern = 0
+
 Area =[
     [' * ', ' * ', ' * '],
     [' * ', ' * ', ' * '],
     [' * ', ' * ', ' * ']
       ]
 Win = False
-def WinnerCheck(win = False):
+def WinnerCheck(win):
     for i in range(3):
         ShotColumn = 0
         ShotString = 0
+
         for g in range(3):
             if Area[i][g] == ' 0 ':
                 ShotColumn += 1
@@ -18,50 +19,59 @@ def WinnerCheck(win = False):
                 ShotString += 1
             if Area[g][i] == ' X ':
                 ShotString -= 1
-        if ShotColumn == -2 or ShotColumn == 2:
-            Print('конец игры ')
-            win = True
-        if ShotString == -2 or ShotString == 2:
-            Print('конец игры ')
-            win = True
+        if ShotColumn == -3 or ShotColumn == 3:
+            print('     конец игры ')
+            return True
+        if ShotString == -3 or ShotString == 3:
+            print('     конец игры ')
+            return True
     if  Area[0][0] == ' X ' and Area [1][1] == ' X ' and Area [2][2] == ' X ' :
-        Print('конец игры ')
-        win = True
+        print('конец игры ')
+        return True
     elif Area[0][2] == ' X ' and Area [1][1] == ' X ' and Area [2][0] == ' X ' :
-        Print('конец игры ')
-        win = True
+        print('конец игры ')
+        return True
     elif Area[0][0] == ' O ' and Area[1][1] == ' O ' and Area[2][2] == ' O ':
-        Print('конец игры ')
-        win = True
+        print('конец игры ')
+        return True
     elif Area[0][2] == ' O ' and Area[1][1] == ' O ' and Area[2][0] == ' O ':
-        Print('конец игры ')
-        win = True
-def Drow_Area():
-    for i in area:
-        print (*i)
-def Tern( tern , win ):
+        print('конец игры ')
+        return True
+def Drow_Area(Area):
+    for i in Area:
+        print ('  ' , *i, '  ')
+        print(' ')
+    print('################################')
+def Tern( tern  , win  ):
     if tern % 2 == 0:
-        Print('Ходят крестики')
-        tern += 1
+        print('Ходят крестики')
+
     else:
-        Print('Ходят  нолики  ')
-        tern += 1
+        print('Ходят  нолики  ')
+
     if tern >=9:
-        win = True
-def InP_Tern(Str_, Col_, tern):
-    Input('Выберете строку(1-3)', Str_)
-    Input('Выберете колонку(1-3)', Col_)
-    Str_ = Str - 1
-    Col_ = Col - 1
-    if Area(Str_, Col_) != ' * ':
-        Print(' Уже занятая ячейка ')
+        return True
+def InP_Tern( tern , Str_ = 0 , Col_ = 0 ):
+    Tern(tern , Win)
+    Str_ = int(input('Выберете строку(1-3)')) - 1
+    Col_ = int(input('Выберете колонку(1-3)'))  - 1
+    if Area[Str_][ Col_] != ' * ':
+        print(' Уже занятая ячейка ')
+        print('################################')
+        return
     elif tern % 2 == 0:
         Area[Str_][Col_]= ' X '
     else:
-        Area[StnerCheck(r_][Col_] = ' O '
-while Win == False:
-    Drow_Area()
-    Tern()
-    InP_Tern()
-    Win = WinnerCheck()
+        Area[Str_][Col_] = ' O '
+    tern += 1
+    print('################################')
+tern = 0
+while True:
+
+    InP_Tern(tern)
+    Win = WinnerCheck(Win)
+    tern += 1
+    Drow_Area(Area)
+    if Win == True:
+        break
 
